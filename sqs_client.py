09232449310e4d8aee02b4e4d7191385eaeb0b94
Magnus Lauritzen Holtet <magnulho@gmail.com>
2021-04-08 18:22:20 +0200

    We're going to move the entire music industry into the 21st Century

--- sqs_client.py
+++ sqs_client.py
@@ -1,4 +1,4 @@
-import boto3
+iimport boto3
 from consts import REGION, QUEUE_NAME, MESSAGE_GROUP_ID
 from datetime import datetime, timezone
 import uuid
@@ -11,9 +11,18 @@ def send_queue_message(message: str):
     return response
 
 
+#"key1: value1; key2: value2;" osv., f.eks. "font-size: 48px; background-color: pink;"
+
 def main():
     print("Velkommen til verdens enkleste SQS-klient.")
     print("Klienten kan avsluttes når som helst ved å holde inne CTRL + C.")
+
+    shrek = 'https://media.tenor.com/images/5187ae066f8332352ca554484f0bc41f/tenor.gif'
+
+    color = input("Hvilken bakgrunnsfarge skal teksten ha? ")
+    size = input("Hvilken størrelse skal teksten ha i pixler? ")
+
+    nickname = input("Skriv inn dit kallenavn:")
     while True:
         print()
         print("Skriv inn meldingen du ønsker å sende til køen. For at meldingen faktisk skal bli sendt må du trykke på enter-tasten.")
@@ -23,10 +32,14 @@ def main():
         msg = {
             "message": user_input, 
             "id": id, 
+            "nickname": nickname,
             "timestamp": timestamp, 
+            "style": f"min-height: 100px; font-size: {size}px; background-image: url({shrek}); background-size: 100px 100px;"
             }
         send_queue_message(json.dumps(msg))
         print("Melding sendt!")
         
 if __name__ == '__main__':
     main()
+
+#"style": f"font-size: {size}px; background-color: {color}; "
